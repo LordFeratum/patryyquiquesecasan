3a55b0742b917026d3d778f02c03265c8bd8bd1b
LordFeratum <lord.feratum@gmail.com>
2016-02-27 16:41:56 +0100

    masteR: -

--- quiqueypatrysecasan/web/models.py
+++ quiqueypatrysecasan/web/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Comentario(models.Model):
+    texto    = models.TextField(max_length=140)
+    invitado = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.texto
+
+
+class Photos(models.Model):
+    imagen   = models.ImageField(upload_to='images/user_img', null=False)
+    invitado = models.ForeignKey(User)
