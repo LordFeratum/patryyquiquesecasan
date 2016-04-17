b7996a0c6cfee3b48c4ac5941abd02ab302ed583
Quique Piña <lord.feratum@gmail.com>
2016-04-17 17:27:56 +0200

    master: minor changes

--- quiqueypatrysecasan/web/models.py
+++ quiqueypatrysecasan/web/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Comentario(models.Model):
+class Comment(models.Model):
     texto    = models.TextField(max_length=140)
     invitado = models.ForeignKey(User)
 
-    def __str__(self):
+    def __unicode__(self):
         return self.texto
+    
 
-
-class Photos(models.Model):
+class Photo(models.Model):
     imagen   = models.ImageField(upload_to='images/user_img', null=False)
     invitado = models.ForeignKey(User)
