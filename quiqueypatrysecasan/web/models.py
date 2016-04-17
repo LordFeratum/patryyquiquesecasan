904bc9cd428cfacd98a183fcf3ac28579f3649d6
Quique Piña <lord.feratum@gmail.com>
2016-04-17 18:01:32 +0200

    master: -

--- quiqueypatrysecasan/web/models.py
+++ quiqueypatrysecasan/web/models.py
@@ -3,12 +3,13 @@ from django.contrib.auth.models import User
 
 
 class Comment(models.Model):
-    texto    = models.TextField(max_length=140)
-    invitado = models.ForeignKey(User)
+    texto         = models.TextField(max_length=140)
+    invitado      = models.ForeignKey(User)
+    acompaniantes = models.IntegerField()
 
     def __unicode__(self):
         return self.texto
-    
+
 
 class Photo(models.Model):
     imagen   = models.ImageField(upload_to='images/user_img', null=False)
